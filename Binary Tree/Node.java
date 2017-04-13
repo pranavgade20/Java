4f0c68b7712689cb863b388151b368a6cd644f91
pranavgade20 <pranavgade20@gmail.com>
2017-04-13 21:11:49 +0530

    Add files via upload

---
+++ Binary Tree/Node.java
@@ -0,0 +1,73 @@
+/**
+ * The class for every node of the tree.
+ *
+ * @author Pranav Gade
+ * @version 0.2
+ */
+public class Node
+{
+    public int value;
+    
+    public Node parent;//the parent node
+    public Node left;//the left child node, if existing
+    public Node right;//the right child node, if existing
+    
+    public Node(int v)//constructor for the base node of the tree
+    {
+        this(v, null);
+    }
+    public Node(int v ,Node n)//constructor for all other nodes. Adds the parent too
+    {
+        value = v;
+        parent = n;
+    }
+    
+    /**
+     * A method to add an element to the tree.
+     * Self-refrencing if the appropriate node is not null.
+     *
+     * @param  n  the value to be added to the tree
+     * @return    Nothing, yet.(as, of course you can see ;-p)
+     */
+    public void addElement(int n)
+    {
+        if(value > n){
+            if(left != null){
+                left.addElement(n);
+            }else{
+                left = new Node(n, this);
+            }
+        }else if(value < n){
+            if(right != null){
+                right.addElement(n);
+            }else{
+                right = new Node(n, this);
+            }
+        }else{
+            System.out.println("Value is the same!");
+        }
+    }
+    
+    /**
+     * A method to search the given element in the left and right nodes, and call itself again.
+     * 
+     * @param  x  the number to search
+     * @return    true or false, the element exists or not
+     */
+    public boolean search(int x)
+    {
+        boolean result = false;
+        if(value == x){
+            result = true;
+        }else{
+            if(right != null && value < x){
+                result = right.search(x);
+            }
+            if(left != null && value > x){
+                result = left.search(x);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ Binary Tree/Tree.java
@@ -0,0 +1,48 @@
+/**
+ * The class tree.This includes addElement.
+ *
+ * @author Pranav Gade
+ * @version 0.1
+ */
+public class Tree
+{
+    public Node base;
+    
+    public int size = 1;//size of the tree.No use at present.:-(
+    public Tree(int v)//construct a tree with a base value of v
+    {
+        base = new Node(v);
+    }
+    
+    /**
+     * A method to add an element to the tree.
+     * Rereferences to addElement form Node.java
+     * also adds one to the size of the tree
+     *
+     * @param  n  the value to be added to the tree
+     * @return    Nothing, yet.(as, of course you can see ;-p)
+     */
+    public void addElement(int n)
+    {
+        base.addElement(n);
+        size++;
+    }
+    
+    public void sort(){//return an sorted array of elements in the tree
+        //Needs serious work :-|          ;-P
+    }
+    
+    /**
+     * A method to search an element in the tree.
+     * If exists, return true, else return false.
+     *
+     * @param  x  an element to search
+     * @return    true or false - x exists or not
+     */
+    public boolean search(int x)
+    {
+        boolean result = base.search(x);
+        
+        return result;
+    }
+}
